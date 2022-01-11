bd769dedd8245e2df88ef9fd5d60a3b20cdd2ac5
h66guo <59834534+h66guo@users.noreply.github.com>
2022-01-11 16:58:12 -0500

    Update uniqueLetterString.py

--- uniqueLetterString.py
+++ uniqueLetterString.py
@@ -1,13 +1,23 @@
-class Solution:
-    def uniqueLetterString(self, s: str) -> int:
-        map1 = collections.defaultdict(lambda:-1)    # last index for this letter
-        map2 = collections.defaultdict(lambda:-1)   # second last index for this letter
-        cur = 0
-        res = 0
+def uniqueLetterString(self, s):
+    res = 0
+    det = 0
+    first_appear = {}
+    second_appear = {}
+    
+    for i in range(len(s)):
+        if s[i] not in first_appear:
+            det = det + i + 1
+            first_appear[s[i]] = i
+            
+        elif s[i] in first_appear and s[i] not in second_appear:
+            det = det + i + 1 - (first_appear[s[i]] + 1) * 2
+            second_appear[s[i]] = i
         
-        for i, ch in enumerate(s):
-            cur += (i - map1[ch]) - (map1[ch] - map2[ch])
-            map2[ch] = map1[ch]
-            map1[ch] = i
-            res += cur
-        return res
+        elif s[i] in first_appear and s[i] in second_appear:
+            det = det + i + 1 - (second_appear[s[i]] - first_appear[s[i]]) * 2 - (first_appear[s[i]] + 1)
+            first_appear[s[i]] = second_appear[s[i]]
+            second_appear[s[i]] = i
+        
+        res += det
+            
+    return res
