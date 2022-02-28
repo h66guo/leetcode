0f2ec5f32d7945dd7d13dee0d294891f6abed832
h66guo <59834534+h66guo@users.noreply.github.com>
2022-02-28 02:25:19 -0500

    Create groupStrings.py

---
+++ groupStrings.py
@@ -0,0 +1,37 @@
+# class Solution:
+#     def groupStrings(self, strings: List[str]) -> List[List[str]]:
+#         def shift_letter(letter, shift):
+#             return chr((ord(letter) -shift)%26 + ord('a'))
+        
+#         def do_hash(string):
+#             shift= ord(string[0])
+#             return ''.join(shift_letter(letter, shift) for letter in string)
+        
+#         groups= collections.defaultdict(list)
+#         for string in strings:
+#             ret= do_hash(string)
+#             groups[ret]=string
+            
+#         return list(groups.values())
+
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        
+        def shift_letter(letter: str, shift: int):
+            return chr((ord(letter) - shift) % 26 + ord('a'))
+        
+        # Create a hash value
+        def get_hash(string: str):
+            # Calculate the number of shifts to make the first character to be 'a'
+            shift = ord(string[0])
+            return ''.join(shift_letter(letter, shift) for letter in string)
+        
+        # Create a hash_value (hashKey) for each string and append the string
+        # to the list of hash values i.e. mapHashToList["abc"] = ["abc", "bcd"]
+        groups = collections.defaultdict(list)
+        for string in strings:
+            hash_key = get_hash(string)
+            groups[hash_key].append(string)
+        
+        # Return a list of all of the grouped strings
+        return list(groups.values())
